8671d8c18e35c4f7bbd07840be603b9c770c2fe2
sammitako <p.hansaem@hotmail.com>
2023-05-02 00:15:30 -0400

    Completed search-engine

--- src/main/java/com/example/recipeapp/post/PostService.java
+++ src/main/java/com/example/recipeapp/post/PostService.java
@@ -6,13 +6,18 @@ import com.example.recipeapp.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.core.query.TextQuery;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,11 +36,27 @@ public class PostService {
     }
 
     public List<Post> searchPosts(String kwd) {
-        TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matching(kwd);
-        Query query = TextQuery.queryText(textCriteria).sortByScore();
+        // Create a regex pattern for the keyword
+        Pattern pattern = Pattern.compile(kwd, Pattern.CASE_INSENSITIVE);
 
-        // Search in Post collection
-        List<Post> posts = mongoTemplate.find(query, Post.class);
+        // Create queries for each field
+        Query titleQuery = new Query(Criteria.where("title").regex(pattern));
+        Query contentQuery = new Query(Criteria.where("content").regex(pattern));
+        Query categoryQuery = new Query(Criteria.where("category").regex(pattern));
+        Query ingredientsQuery = new Query(Criteria.where("ingredients").regex(pattern));
+
+        // Execute the queries
+        List<Post> titlePosts = mongoTemplate.find(titleQuery, Post.class);
+        List<Post> contentPosts = mongoTemplate.find(contentQuery, Post.class);
+        List<Post> categoryPosts = mongoTemplate.find(categoryQuery, Post.class);
+        List<Post> ingredientsPosts = mongoTemplate.find(ingredientsQuery, Post.class);
+
+        // Merge the result lists
+        Set<Post> posts = new HashSet<>();
+        posts.addAll(titlePosts);
+        posts.addAll(contentPosts);
+        posts.addAll(categoryPosts);
+        posts.addAll(ingredientsPosts);
 
         // Search in User collection
         List<User> users = userRepository.findByFirstNameRegexOrLastNameRegex(kwd, kwd);
@@ -46,7 +67,7 @@ public class PostService {
         // Add any posts written by the matching users to the posts list
         posts.addAll(postRepository.findByUserIdIn(userIds));
 
-        return posts;
+        return new ArrayList<>(posts);
     }
 
 
