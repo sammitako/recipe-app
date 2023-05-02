4088ac663068e41380dce880e3b3059da4d3547a
sammitako <p.hansaem@hotmail.com>
2023-05-02 16:35:23 -0400

    UI completed

--- src/main/client/components/CreateButton.jsx
+++ src/main/client/components/CreateButton.jsx
@@ -6,11 +6,91 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import AddIcon from "@mui/icons-material/Add";
-import { useState } from "react";
-import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Autocomplete, Box, Chip } from "@mui/material";
+import { topFilms } from "../libs/data";
+import { useDropzone } from "react-dropzone";
+import { makeStyles } from "@mui/styles";
+import Image from "next/image";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+const useStyles = makeStyles(() => ({
+  dropzone: {
+    border: "2px dotted rgba(0, 0, 0, .6)",
+    padding: "20px",
+    textAlign: "center",
+    marginBottom: "10px",
+    width: "100%",
+    color: "#000000",
+    opacity: 0.6,
+    marginTop: 20,
+  },
+}));
+
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  marginTop: 16,
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: "border-box",
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden",
+  margin: "0 auto",
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
 
 export default function CreateButton() {
   const [open, setOpen] = useState(false);
+  const [file, setFile] = useState([]);
+  const classes = useStyles();
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      setFile(
+        Object.assign(acceptedFiles[0], {
+          preview: URL.createObjectURL(acceptedFiles[0]),
+        })
+      );
+    },
+  });
+
+  const thumbs = file ? (
+    <div style={thumb}>
+      <div style={thumbInner}>
+        <Image
+          alt="cover"
+          src={file.preview}
+          style={img}
+          width={500}
+          height={500}
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </div>
+    </div>
+  ) : null;
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -19,6 +99,11 @@ export default function CreateButton() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => URL.revokeObjectURL(file.preview);
+  }, [file]);
 
   return (
     <Box
@@ -40,25 +125,82 @@ export default function CreateButton() {
         Share your recipe
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Share my recipe</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
+          <DialogContentText sx={{ mb: 2 }}>
+            To share your recipe, you will need a cover photo. Please enter all
+            the ingredients that would be used to make your dish.
           </DialogContentText>
           <TextField
+            sx={{ mb: 2 }}
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
-            type="email"
+            label="Dish Name"
             fullWidth
-            variant="standard"
+            variant="outlined"
           />
+
+          <Autocomplete
+            sx={{ mb: 2 }}
+            disablePortal
+            fullWidth
+            id="combo-box-demo"
+            options={topFilms.map((option) => option.title)}
+            renderInput={(params) => <TextField {...params} label="Category" />}
+          />
+          <Autocomplete
+            sx={{ mb: 2.5 }}
+            multiple
+            id="tags-filled"
+            options={topFilms.map((option) => option.title)}
+            // defaultValue={[topFilms[13].title]}
+            freeSolo
+            renderTags={(value, getTagProps) =>
+              value.map((option, index) => (
+                <Chip
+                  key={option}
+                  variant="outlined"
+                  label={option}
+                  {...getTagProps({ index })}
+                />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Ingredients"
+                placeholder="ingredient"
+              />
+            )}
+          />
+          <TextField
+            fullWidth
+            id="outlined-multiline-flexible"
+            label="Recipe"
+            placeholder="Share your method"
+            multiline
+            rows={10}
+          />
+          <section className="container">
+            <div
+              style={{ cursor: "pointer" }}
+              {...getRootProps({ className: classes.dropzone })}
+            >
+              <input {...getInputProps()} />
+              <p>
+                Drag and drop a cover image file here, or click to select file
+              </p>
+              <CloudUploadIcon sx={{ fontSize: 40 }} />
+            </div>
+            <aside style={{ ...thumbsContainer, justifyContent: "center" }}>
+              {file.preview ? thumbs : null}
+            </aside>
+          </section>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button onClick={handleClose}>Post</Button>
         </DialogActions>
       </Dialog>
     </Box>
--- src/main/client/components/PostCard.jsx
+++ src/main/client/components/PostCard.jsx
@@ -60,6 +60,7 @@ const PostCard = (
         width={500}
         height={500}
         layout="responsive"
+        priority
         src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
         alt="Paella dish"
       />
